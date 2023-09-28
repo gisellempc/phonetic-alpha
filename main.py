4bb8b1d697e816f8a18aedf70a60cf532f68fe5e
gisellempc03@gmail.com <gisellempc03@gmail.com>
2023-09-28 13:47:20 -0400

    Add phonetic alphabet converter

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+keep_asking = True
+while keep_asking:
+    word = input("Enter a word: ").upper()
+
+    if word == "EXIT":
+        keep_asking = False
+    else:
+        phon_word = [data_dict[letter] for letter in word]
+        print(phon_word)
