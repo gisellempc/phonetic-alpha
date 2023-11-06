913b11f1d3b3ab2bb72a92f47e23b9f44fffbbea
gisellempc03@gmail.com <gisellempc03@gmail.com>
2023-11-06 18:13:16 -0400

    Add try/except

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 keep_asking = True
 while keep_asking:
-    word = input("Enter a word: ").upper()
-
-    if word == "EXIT":
-        keep_asking = False
-    else:
-        phon_word = [data_dict[letter] for letter in word]
-        print(phon_word)
+    try:
+        word = input("Enter a word: ").upper()
+        if word == "EXIT":
+            keep_asking = False
+        else:
+            phon_word = [data_dict[letter] for letter in word]
+            print(phon_word)
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
